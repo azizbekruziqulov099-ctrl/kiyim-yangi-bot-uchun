6e94e5fc0367527d9a6a2c8a7552cdebaef17700
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-04-20 08:21:16 +0500

    20.3

--- kiym.py
+++ kiym.py
@@ -1,13 +1,20 @@
 elif text == "💾 Saqlash":
     pid = context.user_data.get("edit_product_id")
 
-    # 🔥 size ni tekshiramiz
+    # size
     size = context.user_data.get("size")
-
     if not size:
         for p in products:
             if p["id"] == pid:
-                size = p["size"]
+                size = p.get("size", "")
+                break
+
+    # count
+    count = context.user_data.get("count")
+    if count is None:
+        for p in products:
+            if p["id"] == pid:
+                count = p.get("count", 0)
                 break
 
     cur.execute("""
@@ -17,12 +24,14 @@ elif text == "💾 Saqlash":
     """, (
         context.user_data.get("gender"),
         context.user_data.get("origin"),
-        ",".join(context.user_data.get("seasons", [])),
+        ",".join(context.user_data.get("seasons") or []),
         context.user_data.get("category"),
-        size,  # 🔥 SHU YER O‘ZGARDI
-        context.user_data.get("count"),
+        size,
+        count,
         pid
     ))
 
     conn.commit()
     load_products_from_db()
+
+    await update.message.reply_text("✅ Saqlandi")
